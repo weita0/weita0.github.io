57fc7a23b25e7e9fd58a201955f4e08c26bbe371
weita0 <weitao907@gmail.com>
2016-02-25 16:32:58 +0800

    update

---
+++ script/anniversary/calculate.js
@@ -0,0 +1,7 @@
+var oneDay = 24 * 60 * 60 * 1000;
+var firstDate = new Date(2014,08,04);
+var secondDate = Date.now();
+
+var diffDays = Math.around(Math.abs((firstDate.getTime() - secondDate.getTime())/oneDay)
+
+$('#days').html(diffDays);
