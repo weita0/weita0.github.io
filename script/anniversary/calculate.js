b089ac2da90ab12a0be5b6d9ac46310f56576751
weita0 <weitao907@gmail.com>
2016-03-06 15:35:00 +0800

    Routine Update

--- script/anniversary/calculate.js
+++ script/anniversary/calculate.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-	var oneDay = 24 * 60 * 60 * 1000;
-	var firstDate = new Date(2014,08,04);
-	var secondDate = Date.now();
-
-	var days_between = Math.round(Math.abs((firstDate.getTime() - secondDate)/oneDay));
-
-	$('#days').html(days_between);
+		
+	var daysBetween = function() {
+		var oneDay = 24 * 60 * 60 * 1000;
+		var firstDate = new Date(2014,08,04);
+		var secondDate = Date.now();
+		var days_between = Math.round(Math.abs((firstDate.getTime() - secondDate)/oneDay));
+		return days_between;
+	};
+	$('#days').html(daysBetween());
 
 });
---
+++ script/common/decoration.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	this.addEventListener('mousemove', function(e) {
+		var x = e.pageX + 'px';
+		var y = e.pageY + 'px';
+		var trail = $('<div></div>', {
+			class: 'trail'
+		});
+		trail.css('left', x);
+		trail.css('top', y);
+		var body = $('body');
+		body.append(trail);
+		makeItDisappear(trail, 500);
+	});
+
+	this.addEventListener('mousedown', function(e) {
+		if(e.which === 1) {
+			var x = e.pageX + 'px';
+			var y = e.pageY + 'px';
+			var heart = $('<div></div>', {
+				class: 'heart'
+			});
+			heart.css('left', x);
+			heart.css('top', y);
+			heart.addClass('fix');
+			var body = $('body');
+			body.append(heart);
+			makeItDisappear(heart, 300);
+		}
+	});
+
+	function makeItDisappear(element, milliSecond) {
+		setTimeout(function(element) {
+			element.remove();
+		}, milliSecond);
+	}
+});
